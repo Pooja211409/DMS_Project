2281d30e25cb182f9ffa0b1ed76ab70ab2128418
poojamkokane <poojamkokane@gmail.com>
2024-07-04 09:57:53 +0530

    test commit

--- src/main/java/com/dms/DmsProjectApplication.java
+++ src/main/java/com/dms/DmsProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DmsProjectApplication {
 
 	public static void main(String[] args) {
-		
+		//test comment
 		SpringApplication.run(DmsProjectApplication.class, args);
 	}
 
